d064496a7ef730b74cfb0180f50e16dbc9bb13d5
tugaybakay <bkytugay@gmail.com>
2023-08-17 00:35:02 +0300

    first commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by MacOS on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
